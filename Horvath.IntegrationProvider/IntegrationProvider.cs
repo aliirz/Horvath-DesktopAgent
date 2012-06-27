4592031b4904f084786ac483474e0cd208ad8580
Ali Raza <dejavu87@gmail.com>
2012-06-27 16:21:45 +0500

    initial commit

---
+++ Horvath.IntegrationProvider/IntegrationProvider.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RestSharp;
+
+namespace Horvath
+{
+    public class IntegrationProvider
+    {
+        private RestClient _client;
+
+        public IntegrationProvider(string url)
+        {
+            this._client = new RestClient(url);
+            this._client.Authenticator = new HttpBasicAuthenticator("yawar", "shah");
+        }
+
+        public bool CheckConnectivity()
+        {
+            bool connected = true;
+            try
+            {   
+        
+                var request = new RestRequest("authenticate", Method.POST);
+                request.Parameters.Add(new Parameter { Name = "username", Value = "yawar" });
+                request.Parameters.Add(new Parameter { Name = "password", Value = "shah" });
+                
+
+                var response = this._client.Execute(request);
+
+                Console.WriteLine(response.Content);
+                connected = true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                connected = false;
+            }
+
+            return connected;
+        }
+    }
+}
---
+++ Horvath.Tests/IntegrationProviderTest.cs
@@ -0,0 +1,82 @@
+﻿using Horvath;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Horvath.Tests
+{
+    
+    
+    /// <summary>
+    ///This is a test class for IntegrationProviderTest and is intended
+    ///to contain all IntegrationProviderTest Unit Tests
+    ///</summary>
+    [TestClass()]
+    public class IntegrationProviderTest
+    {
+
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        // 
+        //You can use the following additional attributes as you write your tests:
+        //
+        //Use ClassInitialize to run code before running the first test in the class
+        //[ClassInitialize()]
+        //public static void MyClassInitialize(TestContext testContext)
+        //{
+        //}
+        //
+        //Use ClassCleanup to run code after all tests in a class have run
+        //[ClassCleanup()]
+        //public static void MyClassCleanup()
+        //{
+        //}
+        //
+        //Use TestInitialize to run code before running each test
+        //[TestInitialize()]
+        //public void MyTestInitialize()
+        //{
+        //}
+        //
+        //Use TestCleanup to run code after each test has run
+        //[TestCleanup()]
+        //public void MyTestCleanup()
+        //{
+        //}
+        //
+        #endregion
+
+
+        /// <summary>
+        ///A test for CheckConnectivity
+        ///</summary>
+        [TestMethod()]
+        public void CheckConnectivityTest()
+        {
+            string url = "http://192.168.100.108:3000"; // TODO: Initialize to an appropriate value
+            IntegrationProvider target = new IntegrationProvider(url); // TODO: Initialize to an appropriate value
+            bool expected = true; // TODO: Initialize to an appropriate value
+            bool actual;
+            actual = target.CheckConnectivity();
+            Assert.AreEqual(expected, actual);
+           // Assert.Inconclusive("Verify the correctness of this test method.");
+        }
+    }
+}
